cf1cba6b6ca6da975905d13935f96c86639656f3
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 15:58:05 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+           System.out.println("Input your birth month using 1 to 12: ");
+           int birthMonth = 2;
+
+           if (birthMonth == 1){
+               System.out.println("January ");
+           } else if ("birthMonth == 2") {
+               System.out.println("February ");
+           } else if (birthMonth == 3) {
+               System.out.println("March ");
+           } else if ("birthMonth == 4") {
+               System.out.println("April" );
+           } else if ("birthMonth == 5") {
+               System.out.println("May" );
+           } else if ("birthMonth == 6") {
+               System.out.println("June ");
+           } else if ("birthMonth == 7") {
+               System.out.println("March");
+           } else if ("birthMonth == 8") {
+               System.out.println("March");
+           } else if ("birthMonth == 9") {
+               System.out.println("March");
+           } else if ("birthMonth == 10") {
+               System.out.println("March");
+           } else if ("birthMonth == 11") {
+               System.out.println("March");
+           } else if ("birthMonth == 12") {
+               System.out.println("March");
+           }
+    }
+}
