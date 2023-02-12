94f0d8f04b99b955925ba65e23dab9eead081181
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 16:08:32 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,34 @@ public class Main {
     public static void main(String[] args) {
 
            System.out.println("Input your birth month using 1 to 12: ");
-           int birthMonth = 2;
+           int birthMonth =15;
 
            if (birthMonth == 1){
                System.out.println("January ");
-           } else if ("birthMonth == 2") {
+           } else if (birthMonth == 2) {
                System.out.println("February ");
            } else if (birthMonth == 3) {
                System.out.println("March ");
-           } else if ("birthMonth == 4") {
+           } else if (birthMonth == 4) {
                System.out.println("April ");
-           } else if ("birthMonth == 5") {
+           } else if (birthMonth == 5) {
                System.out.println("May ");
-           } else if ("birthMonth == 6") {
+           } else if (birthMonth == 6) {
                System.out.println("June ");
-           } else if ("birthMonth == 7") {
+           } else if (birthMonth == 7) {
                System.out.println("July ");
-           } else if ("birthMonth == 8") {
+           } else if (birthMonth == 8) {
                System.out.println("August ");
-           } else if ("birthMonth == 9") {
+           } else if (birthMonth == 9) {
                System.out.println("September ");
-           } else if ("birthMonth == 10") {
+           } else if (birthMonth == 10) {
                System.out.println("October ");
-           } else if ("birthMonth == 11") {
+           } else if (birthMonth == 11) {
                System.out.println("November ");
-           } else if ("birthMonth == 12") {
+           } else if (birthMonth == 12) {
                System.out.println("December ");
            } else
-               System.out.println("Your enter an incorrect Month value: " + birthMonth);
+               System.out.println("You enter an incorrect Month value: " + birthMonth);
 
     }
 }
